7e3a2cd4b64d315389296bad2b05aed1f7026009
feather <Brian608@163.com>
2024-01-06 22:10:27 +0800

    静态内部类 枚举 单例

---
+++ src/main/java/org/feather/creation/GlobalCounter.java
@@ -0,0 +1,21 @@
+package org.feather.creation;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @projectName: design-pattern
+ * @package: org.feather.creation
+ * @className: GlobalCounter
+ * @author: feather
+ * @description: 全局计数器
+ * @since: 06-Jan-24 10:05 PM
+ * @version: 1.0
+ */
+public enum GlobalCounter {
+
+    INSTANCE;
+    private AtomicLong atomicLong=new AtomicLong(0);
+    public  Long getNumber(){
+        return atomicLong.getAndDecrement();
+    }
+}
---
+++ src/main/java/org/feather/creation/Season.java
@@ -0,0 +1,16 @@
+package org.feather.creation;
+
+/**
+ * @projectName: design-pattern
+ * @package: org.feather.creation
+ * @className: Season
+ * @author: feather
+ * @description:
+ * @since: 06-Jan-24 10:04 PM
+ * @version: 1.0
+ */
+public enum Season {
+    //对于枚举类，任何一个枚举项就是一个天然的单例
+    //本质上就是 static final Season spring= new Season();
+    SPRING
+}
